c100bc800969a9b90070c76855900ccfc2fe22ad
Tushar Kashyap <tusharkashyap2342@gmailc.om>
2023-11-14 00:50:26 +0530

    user profile operations added

--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
    
     path('',views.getUsers,name="users"),
     path('profile/',views.getUserProfile,name="user-profile"),
+    path('profile/update/',views.updateUserProfile,name="user-profile-update"),
     
     
     # path('products/',views.getProducts,name="products"),
