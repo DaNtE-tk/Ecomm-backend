653d2e44a133bfc3e84eada029659e2bec6d6b10
Tushar Kashyap <tusharkashyap2342@gmailc.om>
2024-01-06 01:23:37 +0530

    completed client side functionality

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -3,5 +3,8 @@ import base.views.order_views as views
 
 
 urlpatterns = [
-
+    path('add/', views.addOrderItems, name='order-add'),
+    path('myorders/', views.getMyOrders, name='myorders'),
+    path('<str:pk>/', views.getOrderById, name='user-order'),
+    path('<str:pk>/pay/', views.updateOrderToPaid, name='order-pay'),
 ]
