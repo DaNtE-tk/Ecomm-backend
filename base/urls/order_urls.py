6c3f456a20b4c7a597c4e0a07fb147c87e98e64d
Tushar Kashyap <tusharkashyap2342@gmailc.om>
2024-01-08 04:56:16 +0530

    added delivery endpoint

--- base/urls/order_urls.py
+++ base/urls/order_urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('myorders/', views.getMyOrders, name='myorders'),
     path('<str:pk>/', views.getOrderById, name='user-order'),
     path('<str:pk>/pay/', views.updateOrderToPaid, name='order-pay'),
+    path('<str:pk>/deliver/', views.updateOrderToDelivered, name='order-deliver')
 ]
