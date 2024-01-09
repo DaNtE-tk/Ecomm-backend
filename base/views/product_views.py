54f452c423d13db91b294f16a201e03820c7927c
Tushar Kashyap <tusharkashyap2342@gmailc.om>
2024-01-10 01:42:24 +0530

    added pagination logic

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from base.serializer import ProductSerializer
 # from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from base.models import Product, Review
 
@@ -21,14 +22,32 @@ from rest_framework import status
 # @permission_classes([IsAuthenticated])
 def getProducts(request):
     query = request.query_params.get('keyword')
-    print('query: ',query)
+    print('key: ',query)
 
     if query == None:
         query=''
 
     products = Product.objects.filter(name__icontains=query)
+
+    page = request.query_params.get('page')
+    # page = int(page)
+    print('page: ',page)
+    paginator = Paginator(products,4)
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    if page == None:
+        page = 1
+
+    page = int(page)
+
     serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    return Response({'products':serializer.data, 'page':page, 'pages':paginator.num_pages})
 
 
 @api_view(['GET']) 
